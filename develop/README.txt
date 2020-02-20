3e826652516d131d4d93330a1871ca1a919a6b5f
sgalal <ayaka@mail.shn.hk>
2020-02-20 16:54:06 +0800

    Update develop

--- develop/README.txt
+++ develop/README.txt
@@ -13,4 +13,17 @@ Download the following files:
 
 The total size is 627 MB.
 
+Set the environment variable BOT_TOKEN to bot token.
+
 Run python interact_mmi.py --no_cuda.
+
+Dockerfile pip mirror:
+
+RUN pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
+
+This set the pip mirror to aliyun.com
+
+Run with proxy:
+
+REQUEST_KWARGS = { 'proxy_url': 'socks5h://localhost:1080' }
+updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS, use_context=True)
--- develop/interact_mmi.py
+++ develop/interact_mmi.py
@@ -236,11 +236,42 @@ class Talk:
 	def remove_talk(self, talk_id):
 		if talk_id in self.TALK_LIST:
 			del self.TALK_LIST[talk_id]
+			return True
+		else:
+			return False
 
-talk_id = 'ID'
 t = Talk()
-while True:
-	m = input('>>> ')
-	if not m:
-		break
-	print(t.start_talk(talk_id, m))
+
+# Bot
+
+TOKEN = os.environ['BOT_TOKEN']
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+	chat_id = update.effective_chat.id
+	context.bot.send_message(chat_id=chat_id, text='''我是一個 AI。我還不是很會説話，請多多關照。
+我的開發者們能被找到在 [這裏](https://github.com/sugina-dev/gpt2-chatbot)。
+要讓我忘記和你聊過的天的話，使用 /destroy 命令來達到吧。
+下面開始跟你聊天喔。''', parse_mode='Markdown', disable_web_page_preview=True)
+dispatcher.add_handler(CommandHandler('start', start))
+
+def destroy(update, context):
+	chat_id = update.effective_chat.id
+	if t.remove_talk(chat_id):
+		context.bot.send_message(chat_id=chat_id, text='銷毀已完成')
+	else:
+		context.bot.send_message(chat_id=chat_id, text='已無事可做')
+dispatcher.add_handler(CommandHandler('destroy', destroy))
+
+def chat(update, context):
+	chat_id = update.effective_chat.id
+	text = update.message.text
+	reply = t.start_talk(chat_id, text)
+	context.bot.send_message(chat_id=chat_id, text=reply)
+dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
+updater.start_polling()
